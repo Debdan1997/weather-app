af568e1895df3a6211ea22afc5ca8a003a00e0e4
Debdan1997 <dpapan.rik.1996@gmail.com>
2020-04-06 17:42:39 +0530

    setting up for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ const message = document.querySelector('#message')
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location = search.value
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                 console.log(data.error)
